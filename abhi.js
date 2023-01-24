d73dbe65016d48fdabb9b30be27b3d6dab066c56
8792779148 <104629316+8792779148@users.noreply.github.com>
2023-01-24 15:57:43 +0530

    add initial cv website files

---
+++ abhi.js
@@ -0,0 +1,6 @@
+
+            var price1=prompt("enter the first value");
+            var price2=prompt("enter second value");
+            var total=price1*price2;
+
+            document.write( "multiplication of "+price1+" and" + price2+"is"+   total);
